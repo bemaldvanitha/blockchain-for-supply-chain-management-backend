078e093e64cb5b7f2613ecfd0bb35914ce50d726
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-04-29 08:16:42 +0530

    basic block dev

---
+++ blockchain/Block.js
@@ -0,0 +1,25 @@
+const crypto = require("crypto"), SHA256 = message => crypto.createHash("sha256").update(message).digest("hex");
+
+class Block {
+    constructor(timestamp = "", data = []) {
+        this.timestamp = timestamp;
+        this.data = data;
+        this.hash = this.getHash();
+        this.prevHash = ""; // previous block's hash
+        this.nonce = 0;
+    }
+
+    // hash function
+    getHash() {
+        return SHA256(this.prevHash + this.timestamp + JSON.stringify(this.data) + this.nonce);
+    }
+
+    mine(difficulty) {
+        while (!this.hash.startsWith(Array(difficulty + 1).join("0"))) {
+            this.nonce++;
+            this.hash = this.getHash();
+        }
+    }
+}
+
+module.exports = { Block }
