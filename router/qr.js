77dd14b123f4700b5dd98d9223b97504f5c1154f
bemaldvanitha <bdvidanagamachchi@gmail.com>
2023-04-30 14:03:38 +0530

    qr router done ejs need to code

--- router/qr.js
+++ router/qr.js
@@ -2,6 +2,12 @@ const express = require('express');
 const { uuid } = require('uuidv4');
 const QRCode = require('qrcode');
 const mongoose = require('mongoose');
+const ejs = require('ejs');
+const fs = require('fs');
+
+const { ProductOwner } = require('../models/ProductOwner.js');
+const { Product } = require('../models/Product.js');
+const {Blockchain} = require("../blockchain/Blockchain");
 
 const router = express.Router();
 
@@ -20,6 +26,59 @@ router.post('/', async (req, res) => {
 
 router.get('/:ownId/:prodId', async (req, res) => {
     try{
+        const owner = await ProductOwner.findById(req.params.ownId);
+        const product = await Product.find({ productId: req.params.prodId });
+
+        const data = {
+            owner: {
+                brandName: owner.brandName,
+                location: owner.location,
+                contactNumber: owner.contactNumber,
+                contactEmail: owner.contactEmail
+            },
+            product: {
+                name: product[0].name,
+                blockchain: product[0].blockchain,
+                data: []
+            }
+        }
+
+        let allData = [];
+
+        product[0].blockchain.map((block) => {
+            allData.push(block.data);
+        });
+
+        allData.shift();
+        data.product.data = allData;
+        //console.log(data);
+
+        const ProductChain = new Blockchain();
+        ProductChain.clearAndAddBlock(product[0].blockchain);
+
+        if(ProductChain.isValid()){
+
+            const template = fs.readFileSync('./template/index.ejs','utf8');
+            const html = ejs.render(template,data);
+
+            // Set the content type header to text/html
+            res.setHeader('Content-Type', 'text/html');
+
+            // Send the HTML content in the response body
+            res.statusCode = 200;
+            res.end(html);
+        }else{
+
+            const template = fs.readFileSync('./template/error.ejs','utf8');
+            const html = ejs.render(template,{});
+
+            // Set the content type header to text/html
+            res.setHeader('Content-Type', 'text/html');
+
+            // Send the HTML content in the response body
+            res.statusCode = 200;
+            res.end(html);
+        }
 
     }catch (err){
         console.error(err.message);
